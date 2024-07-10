8f2005c5864e545edcce6d08c2348d013588f699
Raphael Dimeck <raphael.dimeck@gmail.com>
2024-07-10 17:09:24 +0200

    chore: Add Home Page component to frontend

---
+++ frontend/src/pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
