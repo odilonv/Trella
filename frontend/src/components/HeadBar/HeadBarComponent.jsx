edbda5e7ead33e4e82659bb7da638e8b34f639e3
odilonv <odilon.vidal@icloud.com>
2024-07-18 21:30:02 +0200

    chore: Update CSS color variables to use new main color instead of main purple

---
+++ frontend/src/components/HeadBar/HeadBarComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ButtonComponent from '../Button/ButtonComponent';
+
+function HeadBarComponent({ title }) {
+    return (
+        <div
+            className='default-container'
+            style={{
+                display: 'flex',
+                width: '100%',
+                padding: '15px',
+                justifyContent: 'space-between',
+                margin: '0 0 15px 0',
+                gap: '10px',
+            }}>
+            <h2>{title}</h2>
+            <div>
+                <ButtonComponent text='Create Board' href='/create-card' margin='0' color={'var(--main-darker-color)'} />
+                <ButtonComponent text='Create Task' href='/create-card' margin='0 0 0 10px' />
+            </div>
+        </div>
+    );
+}
+
+export default HeadBarComponent;
