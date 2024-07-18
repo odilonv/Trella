1b2aa37eebfd360d3d4de84fbbefdf1d17476e5c
Kerrian SALAUN <ks@mobaitec.fr>
2024-07-18 20:00:56 +0200

    Get started enlevé quand user connecté

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import HeadTextComponent from '../../components/HeadText/HeadTextComponent';
 import ButtonComponent from '../../components/Button/ButtonComponent';
 import trello from '../../assets/img/TrelloUICollage.png';
+import { UserContext } from '../../contexts/UserContext'; 
 
 function HomePage() {
+    const { user } = useContext(UserContext); 
+
     return (
-        <div style={{
-            margin: "15px"
-        }}>
-            <div
-                className='default-container home-container'
-            >
+        <div style={{ margin: "15px" }}>
+            <div className='default-container home-container'>
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -19,12 +18,13 @@ function HomePage() {
                     <HeadTextComponent
                         titleText={<div>Stop <span style={{ color: 'red' }}>Trello</span>, now start manage your tasks with <span style={{ color: 'var(--main-color)' }}>Trella</span> !</div>}
                         firstSubText={'Keep everything in the same place—even if your team isn’t.'} />
-                    <ButtonComponent text='Get Started' href='/signUp' margin='50px 0' />
+                    {!user && ( 
+                        <ButtonComponent text='Get Started' href='/signUp' margin='50px 0' />
+                    )}
                 </div>
                 <img src={trello} alt='trello' style={{ width: '50%' }} />
             </div>
-        </div >
-
+        </div>
     );
 }
 
