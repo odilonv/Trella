fd1f7cbd1bbb3384fb0c0e722dec714301abebc4
odilonv <odilon.vidal@icloud.com>
2024-07-19 12:44:14 +0200

    Update HeaderComponent to conditionally render header links based on user authentication

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -3,6 +3,7 @@ import HeadTextComponent from '../../components/HeadText/HeadTextComponent';
 import ButtonComponent from '../../components/Button/ButtonComponent';
 import trello from '../../assets/img/TrelloUICollage.png';
 import { UserContext } from '../../contexts/UserContext';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 function HomePage() {
     const { user } = useContext(UserContext);
@@ -18,9 +19,9 @@ function HomePage() {
                     <HeadTextComponent
                         titleText={<div>Stop <span style={{ color: 'red' }}>Trello</span>, now start manage your cards with <span style={{ color: 'var(--main-color)' }}>Trella</span> !</div>}
                         firstSubText={'Keep everything in the same place—even if your team isn’t.'} />
-                    {!user && (
-                        <ButtonComponent text='Get Started' href='/signUp' margin='50px 0' />
-                    )}
+
+                    <ButtonComponent text={user ? 'Check my boards' : 'Get Started'} href={user ? '/boards' : '/login'} margin='50px 0' endIcon={<ArrowForwardIosRoundedIcon style={{ fill: 'white' }} />} />
+
                 </div>
                 <img src={trello} alt='trello' style={{ width: '50%' }} />
             </div>
