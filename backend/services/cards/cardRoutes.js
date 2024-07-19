a5f4496044c4d81acdccbade4b2d4fe7d4728f55
Esclapez Loïc <loicesclapez@gmail.com>
2024-07-19 15:16:14 +0200

    fix

--- backend/services/cards/cardRoutes.js
+++ backend/services/cards/cardRoutes.js
@@ -1,8 +1,11 @@
+// backend/services/cards/cardRoutes.js
 import express from 'express';
-import { createCard, getCardByUserId, updateCard } from './cardController.js';
+import { createCard, updateCard } from './cardController.js';
+import {getCardsByBoardId} from "../boards/boardController.js";
 
 export const cardRouter = express.Router();
 
+// Routes pour les cards
 cardRouter.post('/', createCard);
-cardRouter.get('/:userId', getCardByUserId);
+cardRouter.get('/board/:boardId', getCardsByBoardId);
 cardRouter.put('/:cardId', updateCard);
