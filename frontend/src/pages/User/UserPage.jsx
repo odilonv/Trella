78d2a617b2992019fc9bff1fbb34bb1969c756f4
Raphael Dimeck <raphael.dimeck@gmail.com>
2024-07-19 11:16:45 +0200

    chore: Remove unused ChangePassword and ForgotPassword pages

--- frontend/src/pages/User/UserPage.jsx
+++ frontend/src/pages/User/UserPage.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { ButtonComponent, ModalComponent, UserInfoComponent, UserInfoEntryComponent } from '../../components';
+import {  ModalComponent, UserInfoComponent, UserInfoEntryComponent } from '../../components';
 import { getLoggedUser, logout, requireLoggedUser } from '../../services/API/ApiUserSession';
 import UserInfoCategoryComponent from "../../components/UserInfo/UserInfoCategoryComponent";
-import { Divider, IconButton } from "@mui/material";
+import { Divider} from "@mui/material";
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
-import LockResetRoundedIcon from '@mui/icons-material/LockResetRounded';
 import IdentityIcon from '@mui/icons-material/PersonRounded';
 import ContactIcon from '@mui/icons-material/CallRounded';
 import SecurityIcon from '@mui/icons-material/LockRounded';
@@ -92,24 +91,6 @@ function UserPage() {
                                 ]
                             }
                         />
-                        <UserInfoComponent
-                            key="security"
-                            headerText="Sécurité"
-                            value={
-                                [
-                                    <div className="password-container">
-                                        <UserInfoEntryComponent
-                                            key="password"
-                                            entryLabel="Mot de passe"
-                                            entryValue={<IconButton
-                                                style={{ color: 'var(--black)' }}
-                                                href="/changePassword"><LockResetRoundedIcon /></IconButton>}
-                                        />
-                                    </div>
-                                ]
-                            }
-                        />
-                        <h5>Tu souhaites rectifier des informations ? Merci de contacter le service client.</h5>
                     </>
                 ) : null}
             </div>
